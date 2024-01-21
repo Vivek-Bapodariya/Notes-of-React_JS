f3ae62c840167afe8d24edd12004855aabf41b87
Vivek-Bapodariya <vtbvtb412@gmail.com>
2024-01-21 20:41:16 +0530

    first commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,20 @@
+
+
+import Chai from "./cahi"
+
+
+
+
+function App() {
+  const username = "learning on chai aur code" 
+
+  return (
+      <>
+       <Chai/>
+       <h1>React learning on youtube | {username}</h1>
+       <p>Test para</p>
+      </>
+  )
+}
+
+export default App
